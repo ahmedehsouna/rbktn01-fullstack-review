2c24f1ae207722dc500bd11176d5c15b869b8e47
Moriarty <ahmedehsouna@yahoo.com>
2019-12-24 11:59:07 +0100

    sorted

--- server/index.js
+++ server/index.js
@@ -29,7 +29,7 @@ app.post('/repos', function (req, res) {
 app.get('/repos', function (req, res) {
   // TODO - your code here!
   // This route should send back the top 25 repos
-  Repo.find({}).limit(25).exec((err , data)=>{
+  Repo.find({}).limit(25).sort({_id : -1}).exec((err , data)=>{
     res.json(data)
 
   })
